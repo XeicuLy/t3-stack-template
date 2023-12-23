b882cec74ffe6587f7329a8ad3127f34c3276240
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2023-12-23 23:11:57 +0900

    💡 : テストコードに関するコメントアウトを追加

--- src/app/test/page.test.tsx
+++ src/app/test/page.test.tsx
@@ -2,16 +2,25 @@ import { fireEvent, render } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
 import Test from './page';
 
+// 'src/app/test/page.tsx'に対するテストを記述
 describe('src/app/test/page.tsx', () => {
+  // 画面の初期表示に関するテストを記述
   describe('画面の初期表示確認', () => {
+    // クリック数の初期表示が「0」であることをテスト
     test('クリック数の初期表示が「0」であること', () => {
+      // TestコンポーネントをレンダリングしてgetByTextを取得
       const { getByText } = render(<Test />);
+      // 'You clicked 0 times'が定義されていることを期待
       expect(getByText('You clicked 0 times')).toBeDefined();
     });
   });
+  // ボタンをクリックした時、クリック数が画面に表示されていることをテスト
   test('ボタンをクリックした時、クリック数が画面に表示されていること', () => {
+    // TestコンポーネントをレンダリングしてgetByTextを取得
     const { getByText } = render(<Test />);
+    // 'Click'をクリックするイベントを発火
     fireEvent.click(getByText('Click'));
+    // 'You clicked 1 times'が定義されていることを期待
     expect(getByText('You clicked 1 times')).toBeDefined();
   });
 });
--- src/test/sum.test.ts
+++ src/test/sum.test.ts
@@ -1,6 +1,8 @@
 import { sum } from '@/lib/test';
 import { expect, test } from 'vitest';
 
+// 1 + 2 は 3 になることをテスト
 test('1 + 2 は 3 になること', () => {
+  // sum(1, 2)が3であることを期待
   expect(sum(1, 2)).toBe(3);
 });
